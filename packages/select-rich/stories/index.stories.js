2025fd2778dd4810d99fd874045d65140ed11825
Thomas Allmer <d4kmor@gmail.com>
2019-06-13 14:18:31 +0200

    chore: alternative with extension of radioGroup

--- packages/select-rich/stories/index.stories.js
+++ packages/select-rich/stories/index.stories.js
@@ -1,11 +1,66 @@
 import { storiesOf, html } from '@open-wc/demoing-storybook';
-import { css } from '@lion/core';
+import { css, LitElement } from '@lion/core';
+
+import { overlays, LocalOverlayController } from '@lion/overlays';
+import { LionRadioGroup } from '@lion/radio-group';
 
 import '../../button/lion-button.js';
 import '../../form/lion-form.js';
 import '../../listbox/lion-listbox.js';
 import '../../listbox/lion-option.js';
 import '../lion-select-rich.js';
+
+export class LionSelectRich extends LitElement {
+  get modelValue() {
+    return this.contentNode.modelValue;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.contentNode = this.querySelector('[slotpoc="content"]');
+    this.invokerNode = this.querySelector('[slotpoc="invoker"]');
+
+    this._popup = overlays.add(
+      new LocalOverlayController({
+        hidesOnEsc: true,
+        hidesOnOutsideClick: false,
+        placement: this.position,
+        contentNode: this.contentNode,
+        invokerNode: this.invokerNode,
+      }),
+    );
+    this._show = () => this._popup.show();
+    this._hide = () => {
+      this._popup.hide();
+    };
+
+    this._toggle = () => {
+      this._popup.toggle();
+    };
+
+    this.invokerNode.addEventListener('click', this._toggle);
+    this.contentNode.addEventListener('checked-value-changed', ev => {
+      this.invokerNode.innerText = ev.target.checkedValue;
+    });
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.invokerNode.removeEventListener('click', this._toggle);
+  }
+
+  render() {
+    return html`
+      <slot></slot>
+    `;
+  }
+}
+
+customElements.define('lion-select-rich2', LionSelectRich);
+
+export class LionListbox extends LionRadioGroup {}
+
+customElements.define('lion-listbox2', LionListbox);
 
 const selectRichDemoStyle = css`
   .demo-listbox {
@@ -18,6 +73,35 @@ const selectRichDemoStyle = css`
 `;
 
 storiesOf('Forms|Select Rich', module)
+  .add(
+    'SELECT',
+    () => html`
+      <lion-form>
+        <form>
+          <lion-select-rich2 name="dinosGroup" label="What are your favourite dinosaurs?">
+            <button slotpoc="invoker">click</button>
+            <lion-listbox2 slotpoc="content" name="foo">
+              <lion-radio
+                name="dinos[]"
+                label="allosaurus"
+                .choiceValue=${'allosaurus'}
+              ></lion-radio>
+              <lion-radio
+                name="dinos[]"
+                label="brontosaurus"
+                .choiceValue=${'brontosaurus'}
+              ></lion-radio>
+              <lion-radio
+                name="dinos[]"
+                label="diplodocus"
+                .modelValue=${{ value: 'diplodocus', checked: true }}
+              ></lion-radio>
+            </lion-listbox2>
+          </lion-select-rich2>
+        </form>
+      </lion-form>
+    `,
+  )
   .add(
     'Default',
     () => html`
